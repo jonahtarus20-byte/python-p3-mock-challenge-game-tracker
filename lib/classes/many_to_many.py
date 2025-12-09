0ae6fc61aa231656f073e16668fb3f3496cebe44
Your Name <youremail@example.com>
2025-12-09 09:49:30 +0300

    finished assignment

--- lib/classes/many_to_many.py
+++ lib/classes/many_to_many.py
@@ -1,34 +1,78 @@
 class Game:
     def __init__(self, title):
-        self.title = title
+        self._title = str(title)
+    
+    @property
+    def title(self):
+        return self._title
+    
+    @title.setter
+    def title(self, value):
+        # ignore any assignment, keep it immutable
+        pass
 
     def results(self):
-        pass
+        return [result for result in Result.all if result.game == self]
 
     def players(self):
-        pass
+        return list({result.player for result in self.results()})
 
     def average_score(self, player):
-        pass
+        results_for_player = [r.score for r in self.results() if r.player == player]
+        if results_for_player:
+            return sum(results_for_player) / len(results_for_player)
+        return 0
+
 
 class Player:
     def __init__(self, username):
-        self.username = username
+        self._username = username
+    
+    @property
+    def username(self):
+        return self._username
+    
+    @username.setter
+    def username(self, value):
+        # only accept strings of length 2-16
+        if isinstance(value, str) and 2 <= len(value) <= 16:
+            self._username = value
+        # else ignore invalid assignments silently
 
     def results(self):
-        pass
+        return [result for result in Result.all if result.player == self]
 
     def games_played(self):
-        pass
+        return list({result.game for result in self.results()})
 
     def played_game(self, game):
-        pass
+        return game in self.games_played()
 
     def num_times_played(self, game):
-        pass
+        return len([result for result in self.results() if result.game == game])
+
 
 class Result:
+    all = []
     def __init__(self, player, game, score):
-        self.player = player
-        self.game = game
-        self.score = score
+        self._player = player
+        self._game = game
+        self._score = score
+        Result.all.append(self)
+    
+    @property
+    def score(self):
+        return self._score
+
+    @property
+    def player(self):
+        return self._player
+
+    @property
+    def game(self):
+        return self._game
+    
+    @score.setter
+    def score(self, value):
+        # ignore assignment, immutable
+        pass
